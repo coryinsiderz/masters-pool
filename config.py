c591012d2e779967e28d211d42d09cbfcab2d616
coryinsiderz <coryinsiderz@gmail.com>
2026-04-08 12:27:41 -0400

    Change picks deadline to 7:40 AM ET

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 class Config:
     DATABASE_URL = os.environ["DATABASE_URL"]
     SECRET_KEY = os.environ["SECRET_KEY"]
-    PICKS_DEADLINE = "2026-04-09T07:30:00-04:00"
+    PICKS_DEADLINE = "2026-04-09T07:40:00-04:00"
     ESPN_POLL_INTERVAL = int(os.environ.get("ESPN_POLL_INTERVAL", 300))
     ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "cory")
     ENABLE_POLLING = os.environ.get("ENABLE_POLLING", "1")
